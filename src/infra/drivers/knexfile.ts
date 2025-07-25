542bb2f2eefcaec68d033c52817ceda7ed725cb6
guilherme giacomo simoes <trintaeoitogc@gmail.com>
2025-07-25 11:49:03 -0300

    build: alterado para variaveis de ambiente
    Signed-off-by: guilherme giacomo simoes <trintaeoitogc@gmail.com>

--- src/infra/drivers/knexfile.ts
+++ src/infra/drivers/knexfile.ts
@@ -1,15 +1,16 @@
 import type { Knex } from "knex";
 import * as dotenv from "dotenv";
-dotenv.config();
+
+dotenv.config({ path: "../../../.env" });
 
 const config: Knex.Config = {
 	client: "mysql2",
 	connection: {
-		host: "localhost",
-		user: "root",
-		password: "rootroot",
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
 		database: "cripto",
-		port: 3306,
+		port: parseInt(process.env.DB_PORT || "3306"),
 	},
 	migrations: {
 		directory: "../migrations",
